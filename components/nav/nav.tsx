036171750ff4ab92dcf6a11fb83c74076a9b2bc8
imanol <imanolrtega@gmail.com>
2024-09-15 22:44:11 -0300

    WIP

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,12 +1,17 @@
+import * as React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
 import { cn } from "@/lib/utils";
-
 import Logo from "@/public/home-logo-audeas.png";
 import { Button } from "../ui/button";
 import { MobileNav } from "./mobile-nav";
 import { getMenuBySlug } from "@/lib/wordpress";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
 
 export const Nav = async ({ className, children, id }: NavProps) => {
   const mainMenu = await getMenuBySlug("main");
@@ -30,37 +35,48 @@ export const Nav = async ({ className, children, id }: NavProps) => {
           className="hover:opacity-75 transition-all flex gap-2 items-center"
           href="/"
         >
-          <h2 className="sr-only">next-wp starter</h2>
+          <h2 className="sr-only">Audeas | Asociación Civil</h2>
           <Image
             src={Logo}
             alt="Logo"
             className="dark:invert"
             width={175}
             height={80}
-          ></Image>
+          />
         </Link>
         {children}
         <div className="flex items-center gap-2">
-          <div className="mx-2 hidden md:flex">
-            {items.map(({ ID, url, slug, title, type }) => {
-              const getUrl = (
-                type: string,
-                url: string,
-                slug: string
-              ): string => {
-                return type === "post_type" ? `pages/${slug}` : url;
-              };
-              const path = getUrl(type, url, slug);
-              return (
-                <Button key={ID} asChild variant="ghost" size="sm">
-                  <Link href={path}>{title}</Link>
-                </Button>
-              );
-            })}
-          </div>
+          {/* Links fuera del NavigationMenuTrigger */}
+          <NavigationMenu>
+            <NavigationMenuList>
+              {items.map(({ ID, url, slug, title, type }) => {
+                const getUrl = (
+                  type: string,
+                  url: string,
+                  slug: string
+                ): string => {
+                  return type === "post_type" ? `pages/${slug}` : url;
+                };
+                const path = getUrl(type, url, slug);
+                return (
+                  <NavigationMenuItem key={ID}>
+                    <NavigationMenuLink asChild>
+                      <Link
+                        href={path}
+                        className="block font-normal select-none space-y-1 rounded-md p-3 leading-none no-underline transition-colors hover:bg-accent hover:text-accent-foreground"
+                      >
+                        {title}
+                      </Link>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                );
+              })}
+            </NavigationMenuList>
+          </NavigationMenu>
+
           <Button
             asChild
-            className="hidden sm:flex bg-audeas hover:bg-audeas/95"
+            className="hidden sm:flex bg-audeas hover:bg-audeas/95 text-base"
           >
             <Link className="text-white" href="/">
               Recursos
