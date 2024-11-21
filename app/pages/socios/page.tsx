4a48bb5a0b2385415387d14539f07ada7a0cd980
imanol <imanolrtega@gmail.com>
2024-11-21 00:03:16 -0300

    WIP

---
+++ app/pages/socios/page.tsx
@@ -0,0 +1,18 @@
+import { Section, Container } from '@/components/craft'
+import BackButton from '@/components/back'
+
+import Logos from '@/components/logos/logos'
+
+export default async function Page() {
+  return (
+    <Section>
+      <Container className="md:pb-2">
+        <BackButton />
+        <h1 className="md:pt-12">Socios</h1>
+      </Container>
+      <Container>
+        <Logos showTitle={false} />
+      </Container>
+    </Section>
+  )
+}
--- components/logos/logos.tsx
+++ components/logos/logos.tsx
@@ -1,13 +1,15 @@
 import { Section, Container } from '../craft'
 import CarouselSpacing from '../ui/logos'
 
-export default function Logos() {
+export default function Logos({ showTitle = true }) {
   return (
     <Section>
       <Container>
-        <h3 className="text-center !mt-0 mb-2 md:text-3xl md:font-semibold">
-          Socios
-        </h3>
+        {showTitle && (
+          <h3 className="text-center !mt-0 mb-2 md:text-3xl md:font-semibold">
+            Socios
+          </h3>
+        )}
         <CarouselSpacing />
       </Container>
     </Section>
