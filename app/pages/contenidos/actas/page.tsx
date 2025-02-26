bb3e7ec080c9f1d49c61690d069aabf670a71293
imanol <imanolrtega@gmail.com>
2025-02-26 18:28:27 -0300

    WIP

--- app/pages/contenidos/actas/page.tsx
+++ app/pages/contenidos/actas/page.tsx
@@ -4,38 +4,193 @@ import { TableDemo } from '@/components/table/table'
 
 const data = [
   {
-    año: '2018',
+    año: '2020',
     cantidad: '3',
-    link: 'pages/contenidos/2018-2',
+    link: 'pages/contenidos/2020-descargar',
   },
   {
     año: '2019',
-    cantidad: '2',
+    cantidad: '3',
     link: '#',
   },
   {
-    año: '2020',
-    cantidad: '2',
+    año: '2018',
+    cantidad: '3',
+    link: 'pages/contenidos/2018-descargar',
+  },
+  {
+    año: '2017',
+    cantidad: '',
     link: '#',
   },
   {
-    año: '2021',
-    cantidad: '2',
+    año: '2016',
+    cantidad: '',
     link: '#',
   },
   {
-    año: '2022',
-    cantidad: '1',
+    año: '2015',
+    cantidad: '',
     link: '#',
   },
   {
-    año: '2023',
-    cantidad: '1',
+    año: '2014',
+    cantidad: '',
     link: '#',
   },
   {
-    año: '2024',
-    cantidad: '1',
+    año: '2013',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2012',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2011',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2010',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2009',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2008',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2007',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2006',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2005',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2004',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2003',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2002',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2001',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '2000',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1999',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1998',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1997',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1996',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1995',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1994',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1993',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1992',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1991',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1990',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1989',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1988',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1987',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1986',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1985',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1984',
+    cantidad: '',
+    link: '#',
+  },
+  {
+    año: '1983',
+    cantidad: '',
     link: '#',
   },
 ]
