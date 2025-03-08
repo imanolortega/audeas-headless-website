798970f2046f7ab05b2f0e4663bf844377ca7246
imanol <imanolrtega@gmail.com>
2025-03-08 10:36:26 -0300

    Add carousel images

--- lib/data.ts
+++ lib/data.ts
@@ -74,24 +74,24 @@ export const slidesData = [
     title: 'Conocé AUDEAS',
     description:
       'Somos una Asociación Civil que une a las principales instituciones universitarias de la Argentina en Educación Agropecuaria y Forestal',
-    src: 'https://keystoneacademic-res.cloudinary.com/image/upload/element/18/181210_IMG-8694ef98184cdfa0ab52f2afe31d8f99-V1.jpg',
+    src: '/carousel/audeas-carousel-one.jpg',
   },
   {
     title: 'Colaboración y Coordinación entre Instituciones',
     description:
       'Promovemos la colaboración y coordinación entre instituciones para mejorar la calidad y planificación de la Enseñanza Superior Agropecuaria y Forestal',
-    src: 'https://images.unsplash.com/photo-1721137287642-43b251bd6f00?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: '/carousel/audeas-carousel-two.jpg',
   },
   {
     title: 'Impulsamos el Crecimiento Profesional',
     description:
       'Facilitamos el intercambio científico y académico para fortalecer la formación y el desarrollo profesional en ciencias Agropecuarias y Forestales',
-    src: 'https://images.unsplash.com/photo-1507730690594-f21182eee8b1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: '/carousel/audeas-carousel-three.jpg',
   },
   {
     title: 'Potenciamos la Educación Agropecuaria y Forestal',
     description:
       'Funcionamos como un nexo confiable que potencia la Educación Agropecuaria y Forestal, agrupando a Universidades oficialmente reconocidas en nuestro país',
-    src: 'https://keystoneacademic-res.cloudinary.com/image/upload/f_auto/q_auto/g_auto/w_780/dpr_2.0/element/12/124334_shutterstock_244469683.jpg',
+    src: '/carousel/audeas-carousel-four.jpg',
   },
 ]
